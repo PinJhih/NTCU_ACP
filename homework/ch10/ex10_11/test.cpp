bc2f020a1af14bfbf1304b725d7d6eb162b29d92
PinJhih <isaacwang29@gmail.com>
2022-04-27 14:43:41 +0800

    style: Reformat.

--- homework/ch10/ex10_11/test.cpp
+++ homework/ch10/ex10_11/test.cpp
@@ -1,36 +1,37 @@
 // Polynomial test program.
-#include <iostream> 
+#include <iostream>
+
 #include "Polynomial.h"
 using namespace std;
 
 int main() {
-   Polynomial a;
-   Polynomial b;
+	Polynomial a;
+	Polynomial b;
 
-   a.enterTerms();
-   b.enterTerms();
-   cout << "First polynomial is: " << a.toString();
-   cout << "\nSecond polynomial is: " << b.toString() << "\n";
+	a.enterTerms();
+	b.enterTerms();
+	cout << "First polynomial is: " << a.toString();
+	cout << "\nSecond polynomial is: " << b.toString() << "\n";
 
-   Polynomial t{a}; // save the original value of a
-   Polynomial c{a + b};
-   cout << "\nc = a + b yields: " << c.toString();
-   
-   a += b;
-   cout << "\n\na += b yields: " << a.toString();
+	Polynomial t{a};  // save the original value of a
+	Polynomial c{a + b};
+	cout << "\nc = a + b yields: " << c.toString();
 
-   a = t;  // reset a to original value
-   c = a - b;
-   cout << "\n\nc = a - b yields: " << c.toString();
+	a += b;
+	cout << "\n\na += b yields: " << a.toString();
 
-   a -= b;
-   cout << "\n\na -= b yields: " << a.toString();
-   
-   a = t;  // reset a to original value
-   c = a * b;
-   cout << "\n\nc = a * b yields: " << c.toString();
-   
-   a *= b;
-   cout << "\n\na *= b the polynomials yields: " << a.toString();
-   cout << endl;
-} 
+	a = t;  // reset a to original value
+	c = a - b;
+	cout << "\n\nc = a - b yields: " << c.toString();
+
+	a -= b;
+	cout << "\n\na -= b yields: " << a.toString();
+
+	a = t;  // reset a to original value
+	c = a * b;
+	cout << "\n\nc = a * b yields: " << c.toString();
+
+	a *= b;
+	cout << "\n\na *= b the polynomials yields: " << a.toString();
+	cout << endl;
+}
