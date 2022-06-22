e4b5b4ee9a4a3262aef3f108f5e02dfa855d15ef
PinJhih <isaacwang29@gmail.com>
2022-06-22 11:40:53 +0800

    ex14_10

--- ch14/ex14_10/ex14_10.cpp
+++ ch14/ex14_10/ex14_10.cpp
@@ -7,84 +7,110 @@
 
 using namespace std;
 
-void init() {
-	fstream f{"person_info.dat", ios::out | ios::binary};
+void init(fstream &f) {
 	for (int i = 0; i != 100; i++) {
 		Person p;
 		f.seekp(i * sizeof(Person));
 		f.write((const char *)(&p), sizeof(Person));
 	}
 
-	while (1) {
-		int num;
-		cout << "Enter a number(0-99, -1 end the input): ";
-		cin >> num;
-		if (num == -1) {
-			break;
-		} else if (num >= 0 and num <= 99) {
-			string firstName, lastName;
-			int age;
-			cout << "Enter first name, last name and age: ";
-			cin >> firstName >> lastName >> age;
-			Person p{num, age, lastName, firstName};
-			f.seekp(num * sizeof(Person));
-			f.write((const char *)(&p), sizeof(Person));
+	for (int i = 0; i != 10; i++) {
+		int id = -1, age;
+		string firstName, lastName;
+		while (id < 0 or id > 99) {
+			cout << "Enter a number(0~99): ";
+			cin >> id;
 		}
+		cout << "Enter firstName and lastName: ";
+		cin >> firstName >> lastName;
+		cout << "Enter age: ";
+		cin >> age;
+		Person p(id, age, lastName, firstName);
+		f.seekp(id * sizeof(Person));
+		f.write((const char *)(&p), sizeof(Person));
 	}
 }
 
-int main() {
-	while (1) {
-		char cmd;
-		cout << "Should the file be initialized (Y or N): ";
-		cin >> cmd;
+void del(fstream &f) {
+	int id = -1;
+	while (id < 0 or id > 99) {
+		cout << "Enter a number(0~99): ";
+		cin >> id;
+	}
+	Person p;
+	f.seekg(id * sizeof(Person));
+	f.read(reinterpret_cast<char *>(&p), sizeof(Person));
+	if (p.getId() == -1) {
+		cout << "No info." << endl;
+	} else {
+		p.setId(-1);
+		f.seekp(id * sizeof(Person));
+		f.write((const char *)(&p), sizeof(Person));
+	}
+}
 
-		if (cmd == 'y' or cmd == 'Y') {
-			init();
-			break;
-		} else if (cmd == 'n' or cmd == 'N') {
-			break;
-		}
+void update(fstream &f) {
+	int id = -1, age;
+	string firstName, lastName;
+	while (id < 0 or id > 99) {
+		cout << "Enter a number(0~99): ";
+		cin >> id;
 	}
 
-	fstream file{"person_info.dat", ios::in | ios::out | ios::binary};
-
-	for (int i = 0; i != 10; i++) {
-		int num;
-		while (1) {
-			cout << "Enter a number(0-99): ";
-			cin >> num;
-			if (num >= 0 and num <= 99)
-				break;
-		}
-
-		Person temp;
-		file.seekg(num * sizeof(Person));
-		file.read((char *)(&temp), sizeof(Person));
-
-		if (temp.getId() == -1) {
-			cout << "No info" << endl;
-			continue;
-		}
-
-		string firstName, lastName;
-		int age;
-		cout << "Enter first name, last name and age: ";
-		cin >> firstName >> lastName >> age;
-		Person p{num, age, lastName, firstName};
-		file.seekp(num * sizeof(Person));
-		file.write((const char *)(&p), sizeof(Person));
+	Person t;
+	f.seekg(id * sizeof(Person));
+	f.read(reinterpret_cast<char *>(&t), sizeof(Person));
+	if (t.getId() == -1) {
+		cout << "No info" << endl;
+		return;
 	}
 
-	cout << endl;
-	file.clear();
-	file.seekg(0);
-	while (!file.eof()) {
+	cout << "Enter firstName and lastName: ";
+	cin >> firstName >> lastName;
+	cout << "Enter age: ";
+	cin >> age;
+	Person p(id, age, lastName, firstName);
+	f.seekp(id * sizeof(Person));
+	f.write((const char *)(&p), sizeof(Person));
+}
+
+void listAll(fstream &f) {
+	f.seekg(0);
+	while (!f.eof()) {
 		Person p;
-		file.read((char *)(&p), sizeof(Person));
-		if (p.getId() == -1)
-			continue;
-		cout << p.getFirstName() << ' ' << p.getLastName() << ' ' << p.getAge()
-			 << endl;
+		f.read(reinterpret_cast<char *>(&p), sizeof(Person));
+		if (p.getId() != -1) {
+			cout << p.getFirstName() << ' ' << p.getLastName() << ' '
+				 << p.getAge() << endl;
+		}
+	}
+	cout << endl;
+}
+
+int main() {
+	fstream file{"person_info.dat", ios::in | ios::out | ios::binary};
+	init(file);
+
+	while (1) {
+		cout << "1 - Delete a record\n"
+			 << "2 - Update a record\n"
+			 << "3 - List records\n"
+			 << "4 - End the program\n";
+		int cmd;
+		cin >> cmd;
+		if (cmd == 4)
+			break;
+
+		switch (cmd) {
+			case 1:
+				del(file);
+				break;
+			case 2:
+				update(file);
+				break;
+			case 3:
+				listAll(file);
+		}
+		file.clear();
 	}
 }
